9115308dd252f42225c8deb1d74a244ff0c2f570
Elisa <elisalok413@gmail.com>
2025-03-11 23:39:34 +1300

    fea: add user controller

---
+++ user-service/app/Http/Controllers/UserController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use ChannelCredentialsTest;
+use Illuminate\Http\Request;
+use User\UserServiceClient;
+use User\RegisterUserRequest;
+use User\LoginUserRequest;
+use User\LogoutUserRequest;
+use User\GetUserRequest;
+use Grpc\ChannelCredentials;
+
+class UserController extends Controller
+{
+    private $client;
+
+    public function __construct()
+    { 
+        $this->client = new UserServiceClient('localhost:50051', [
+            'credentials' => ChannelCredentials::createInsecure(),
+        ]);
+    }
+
+    /**
+     * Register a new user.
+     */
+    public function register(Request $request)
+    {
+        $grpcRequest = new RegisterUserRequest();
+        $grpcRequest->setName($request->name);
+        $grpcRequest->setEmail($request->email);
+        $grpcRequest->setPassword($request->password);
+
+        list($response, $status) = $this->client->RegisterUser($grpcRequest)->wait();
+        return response()->json([
+            'user_id' => $response->getUserId(),
+            'message' => $response->getMessage(),
+            'error_details' => $response->getErrorDetails()
+        ], $status->code);
+    }
+
+    /**
+     * Login a user.
+     */
+    public function login(Request $request)
+    {
+        $grpcRequest = new LoginUserRequest();
+        $grpcRequest->setEmail($request->email);
+        $grpcRequest->setPassword($request->password);
+
+        list($response, $status) = $this->client->LoginUser($grpcRequest)->wait();
+        return response()->json([
+            'user_id' => $response->getUserId(),
+            'message' => $response->getMessage(),
+            'remember_token' => $response->getRememberToken()
+        ], $status->code);
+    }
+
+    /**
+     * Logout a user.
+     */
+    public function logout(Request $request)
+    {
+        $grpcRequest = new LogoutUserRequest();
+        $grpcRequest->setUserId($request->user_id);
+
+        list($response, $status) = $this->client->LogoutUser($grpcRequest)->wait();
+        return response()->json(['message' => $response->getMessage()], $status->code);
+    }
+
+    /**
+     * Get user details.
+     */
+    public function getUser($id)
+    {
+        $grpcRequest = new GetUserRequest();
+        $grpcRequest->setUserId($id);
+
+        list($response, $status) = $this->client->GetUser($grpcRequest)->wait();
+        return response()->json([
+            'user_id' => $response->getUserId(),
+            'name' => $response->getName(),
+            'email' => $response->getEmail(),
+            'created_at' => $response->getCreatedAt(),
+            'updated_at' => $response->getUpdatedAt(),
+            'message' => $response->getMessage()
+        ], $status->code);
+    }
+}
